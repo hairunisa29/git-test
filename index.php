152afc678a5c08152db8f0912c114dea85c4d191
hairunisa29 <hairunisa2909@outlook.com>
2020-10-05 18:45:48 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
-1.	<!DOCTYPE html>
-2.	<html lang="en">
-3.	<head>
-4.	    <meta charset="UTF-8">
-5.	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-6.	    <title>Latihan GIT remote </title>
-7.	</head>
-8.	<body>
-9.	    <h1> Hello world </h1>
-10.	</body>
-11.	</html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Latihan GIT remote </title>
+</head>
+<body>
+	<h1> Hello world </h1>
+    <p> Shine dream smile, let us light up the night </p>
+</body>
+</html>
