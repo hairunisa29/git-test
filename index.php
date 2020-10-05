aff20e655fb3496687afef0409528145712cc34c
hairunisa29 <hairunisa2909@outlook.com>
2020-10-05 18:41:08 +0700

    menambahkan file index php

---
+++ index.php
@@ -0,0 +1,11 @@
+1.	<!DOCTYPE html>
+2.	<html lang="en">
+3.	<head>
+4.	    <meta charset="UTF-8">
+5.	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+6.	    <title>Latihan GIT remote </title>
+7.	</head>
+8.	<body>
+9.	    <h1> Hello world </h1>
+10.	</body>
+11.	</html>
